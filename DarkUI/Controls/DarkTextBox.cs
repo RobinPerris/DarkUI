0d46ab27902c4bc738965f20ba00b963b1d54fee
Robin <robinperris@gmail.com>
2015-09-18 10:36:50 +0100

    DarkTextBox control

---
+++ DarkUI/Controls/DarkTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace DarkUI
+{
+    public class DarkTextBox : TextBox
+    {
+        #region Constructor Region
+
+        public DarkTextBox()
+        {
+            BackColor = Colors.LightBackground;
+            ForeColor = Colors.LightText;
+            Padding = new Padding(2, 2, 2, 2);
+            BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        #endregion
+    }
+}
