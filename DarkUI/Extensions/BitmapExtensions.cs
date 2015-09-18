2246e6e645364fc62c5118702cd2b55357045dc7
Robin <robinperris@gmail.com>
2015-09-18 09:46:52 +0100

    ToolStrip & Menu renderers
    Added toolstrip & menu renderers. Added extension methods for changing
    bitmap colours and upgraded to .NET 4.0 for easier implementation of
    these methods.

---
+++ DarkUI/Extensions/BitmapExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace DarkUI
+{
+    public static class BitmapExtensions
+    {
+        public static Bitmap SetColor(this Bitmap bitmap, Color color)
+        {
+            var newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var pixel = bitmap.GetPixel(i, j);
+                    if (pixel.A > 0)
+                        newBitmap.SetPixel(i, j, color);
+                }
+            }
+            return newBitmap;
+        }
+
+        public static Bitmap ChangeColor(this Bitmap bitmap, Color oldColor, Color newColor)
+        {
+            var newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var pixel = bitmap.GetPixel(i, j);
+                    if (pixel == oldColor)
+                        newBitmap.SetPixel(i, j, newColor);
+                }
+            }
+            return newBitmap;
+        }
+    }
+}
