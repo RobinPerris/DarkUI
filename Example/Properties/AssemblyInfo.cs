e6056f630ac67dd95cc9255bd78ae3b10e1dfde2
Robin <robinperris@gmail.com>
2016-01-05 13:51:36 +0000

    Fixed typo in assembly info.

--- Example/Properties/AssemblyInfo.cs
+++ Example/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dark UI Example")]
-[assembly: AssemblyDescription("Examlpe application for Dark UI")]
+[assembly: AssemblyDescription("Example application for Dark UI")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robin Perris")]
 [assembly: AssemblyProduct("Dark UI")]
