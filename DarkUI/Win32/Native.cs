710c2e9f84d735c4b6c5c783aa9493e0d9c700d9
ActuallyaDeviloper <ActuallyaDeviloper@users.noreply.github.com>
2018-03-13 13:54:50 +0100

    Provide a reusable control to allow editing sound infos As of this commit it is also used to implement the fixed sound info editor. (which is trivial apart from the sound info control.)

--- DarkUI/Win32/Native.cs
+++ DarkUI/Win32/Native.cs
@@ -11,5 +11,15 @@ namespace DarkUI.Win32
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         internal static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32")]
+        internal static extern IntPtr GetWindow(IntPtr hWnd, int wCmd);
+
+        internal const int GW_CHILD = 5;
+
+        [DllImport("user32")] [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool ShowWindow(IntPtr hWnd, int wCmd);
+
+        internal const int SW_HIDE = 0;
     }
 }
