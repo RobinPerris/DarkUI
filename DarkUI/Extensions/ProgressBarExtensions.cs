77451cf7e2b70035c64570e34f19563987900a83
ActuallyaDeviloper <ActuallyaDeviloper@users.noreply.github.com>
2018-10-10 19:31:50 +0200

    Fix display of progress bar on Windows 8.1 on the animated textures dialog.

---
+++ DarkUI/Extensions/ProgressBarExtensions.cs
@@ -0,0 +1,31 @@
+using System.Windows.Forms;
+
+namespace DarkUI.Extensions
+{
+    // https://stackoverflow.com/questions/6071626/progressbar-is-slow-in-windows-forms
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Sets the progress bar value, without using 'Windows Aero' animation.
+        /// This is to work around a known WinForms issue where the progress bar
+        /// is slow to update.
+        /// </summary>
+        public static void SetProgressNoAnimation(this ProgressBar pb, int value)
+        {
+            // To get around the progressive animation, we need to move the
+            // progress bar backwards.
+            if (value == pb.Maximum)
+            {
+                // Special case as value can't be set greater than Maximum.
+                pb.Maximum = value + 1;     // Temporarily Increase Maximum
+                pb.Value = value + 1;       // Move past
+                pb.Maximum = value;         // Reset maximum
+            }
+            else
+            {
+                pb.Value = value + 1;       // Move past
+            }
+            pb.Value = value;               // Move to correct value
+        }
+    }
+}
