1f1d367c07aec8b03cdf079e1b4d7a67c4f3d57e
Robin Perris <RobinPerris@users.noreply.github.com>
2020-01-10 15:46:43 +0000

    Added DropdownList example

--- Example/Forms/Docking/DockProperties.cs
+++ Example/Forms/Docking/DockProperties.cs
@@ -1,4 +1,5 @@
-﻿using DarkUI.Docking;
+﻿using DarkUI.Controls;
+using DarkUI.Docking;
 
 namespace Example
 {
@@ -9,6 +10,16 @@ namespace Example
         public DockProperties()
         {
             InitializeComponent();
+
+            // Build dummy dropdown data
+            cmbList.Items.Add(new DarkDropdownItem("Item1"));
+            cmbList.Items.Add(new DarkDropdownItem("Item2"));
+            cmbList.Items.Add(new DarkDropdownItem("Item3"));
+            cmbList.Items.Add(new DarkDropdownItem("Item4"));
+            cmbList.Items.Add(new DarkDropdownItem("Item5"));
+            cmbList.Items.Add(new DarkDropdownItem("Item6"));
+
+            cmbList.SelectedItemChanged += delegate { System.Console.WriteLine($"Item changed to {cmbList.SelectedItem.Text}"); };
         }
 
         #endregion
