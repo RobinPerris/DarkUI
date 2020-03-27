79db58c28e4ad5752395116c73c8958a3c9b3159
Robin Perris <RobinPerris@users.noreply.github.com>
2020-03-27 10:44:33 +0000

    Updated assembly info

--- DarkUI/Properties/AssemblyInfo.cs
+++ DarkUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dark UI")]
+[assembly: AssemblyTitle("DarkUI")]
 [assembly: AssemblyDescription("Dark themed control and docking library for .NET WinForms.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robin Perris")]
-[assembly: AssemblyProduct("Dark UI")]
+[assembly: AssemblyProduct("DarkUI")]
 [assembly: AssemblyCopyright("Copyright © Robin Perris")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
